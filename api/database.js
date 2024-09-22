ed09d35fa204f61d917d1f3c2f96f1eecb0a72bb
toasty <okiuhm77@gmail.com>
2024-09-22 21:00:50 +0200

    kalender & etc

---
+++ api/database.js
@@ -0,0 +1,12 @@
+import {createClient} from '@supabase/supabase-js';
+
+
+
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_APIKEY
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+
+function checkdata()
+{}
